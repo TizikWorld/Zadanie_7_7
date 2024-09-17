2f44e7836ff44a078218606014322d074d7f8229
Tiz <dannilo@bk.ru>
2024-09-17 11:01:30 +0400

    Start

--- Program.cs
+++ Program.cs
@@ -4,7 +4,48 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("hi");
         }
+
+        abstract class Delivery
+        {
+            public string Address;
+        }
+
+        class HomeDelivery : Delivery
+        {
+            /* ... */
+        }
+
+        class PickPointDelivery : Delivery
+        {
+            /* ... */
+        }
+
+        class ShopDelivery : Delivery
+        {
+            /* ... */
+        }
+
+        class Order<TDelivery,
+        TStruct> where TDelivery : Delivery
+        {
+            public TDelivery Delivery;
+
+            public int Number;
+
+            public string Description;
+
+            public void DisplayAddress()
+            {
+                Console.WriteLine(Delivery.Address);
+            }
+
+            // ... Другие поля
+        }
+
+
+
+
     }
 }
